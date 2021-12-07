45c4d475564dfcf5eb479ec3b6fa0daac2df9e57
Alexander Hurtado <alexanderjhurtado@gmail.com>
2021-12-06 16:50:19 -0800

    Remove edge attributes + add article embeddings to nodes

--- generate_graph_with_features.py
+++ generate_graph_with_features.py
@@ -5,6 +5,7 @@ import json
 G = nx.read_gml("data_from_paper/graph.gml")
 nodes = pd.read_csv("data_from_snap/articles.tsv", sep="\t", header=None)
 cat_embeds = pd.read_csv("category_embeddings.tsv", sep="\t", header=None)
+article_embeds = pd.read_csv('data_from_paper/article_embeddings.txt', sep=" ", header=None).iloc[: , :-1]
 
 new_node_features = {}
 with open("data_from_paper/nodes.txt") as file:
@@ -15,7 +16,8 @@ with open("data_from_paper/nodes.txt") as file:
 		node_id, out_degree, in_degree = int(line[0]), float(line[1]), float(line[2])
 		node_label = nodes.iloc[node_id][0]
 		cat_embedding = json.loads(cat_embeds.loc[cat_embeds[0] == node_label][1].item())
-		new_node_features[node_label] = { "out_degree": out_degree, "in_degree": in_degree, "category_multi_hot": cat_embedding }
+		art_embed = article_embeds.iloc[node_id].to_list()
+		new_node_features[node_label] = { "out_degree": out_degree, "in_degree": in_degree, "category_multi_hot": cat_embedding, "article_embed": art_embed }
 		if "category0" in G.nodes[node_label]:
 			del G.nodes[node_label]["category0"]
 		if "category1" in G.nodes[node_label]:
@@ -29,14 +31,14 @@ with open("data_from_paper/nodes.txt") as file:
 			del G.nodes[node_label][key]
 nx.set_node_attributes(G, new_node_features)
 
-new_edge_features = {}
-with open("data_from_paper/edges.txt") as file:
-	for line in file:
-		line = line.strip().split("\t")
-		src_node_id, dst_node_id, tf_idf, num_link_clicked = int(line[1]), int(line[2]), float(line[3]), float(line[4])
-		src_node_label, dst_node_label = nodes.iloc[src_node_id][0], nodes.iloc[dst_node_id][0]
-		new_edge_features[(src_node_label, dst_node_label)] = { "tf_idf": tf_idf, "num_link_clicked": num_link_clicked }
-		del G.edges[(src_node_label, dst_node_label)]['weight']
-nx.set_edge_attributes(G, new_edge_features)
+# new_edge_features = {}
+# with open("data_from_paper/edges.txt") as file:
+# 	for line in file:
+# 		line = line.strip().split("\t")
+# 		src_node_id, dst_node_id, tf_idf, num_link_clicked = int(line[1]), int(line[2]), float(line[3]), float(line[4])
+# 		src_node_label, dst_node_label = nodes.iloc[src_node_id][0], nodes.iloc[dst_node_id][0]
+# 		new_edge_features[(src_node_label, dst_node_label)] = { "tf_idf": tf_idf, "num_link_clicked": num_link_clicked }
+# 		del G.edges[(src_node_label, dst_node_label)]['weight']
+# nx.set_edge_attributes(G, new_edge_features)
 
 nx.write_gml(G, "graph_with_features.gml")
